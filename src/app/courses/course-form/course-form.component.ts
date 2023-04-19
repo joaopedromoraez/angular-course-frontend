0b0b39944fddeab7d01f439a13680ba8abcc13bd
João Pedro Moraes <joaopedromoraes@outlook.com>
2023-04-19 10:23:35 -0300

    feat: type formbuilder

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
@@ -12,19 +12,17 @@ import { Location } from '@angular/common';
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location,
-  ) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    })
-  }
+  ) { }
 
   onSubmit() {
     this.service.save(this.form.value)
