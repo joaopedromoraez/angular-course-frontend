88277d6ba44aaadc5a67e791ad5a1be3d8af124c
João Pedro Moraes <joaopedromoraes@outlook.com>
2023-04-19 22:49:00 -0300

    feat: move courses list to component

--- src/app/courses/components/courses-list/courses-list.component.ts
+++ src/app/courses/components/courses-list/courses-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Course } from '../model/course';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-courses-list',
@@ -10,16 +9,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CoursesListComponent {
 
   @Input() courses: Course[] = [];
+  @Output() add = new EventEmitter(false)
 
   readonly displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
-
   onAdd() {
-    this.router.navigate(['new'], { relativeTo: this.route });
+    this.add.emit();
   }
 
 }
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -16,7 +16,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
   declarations: [
