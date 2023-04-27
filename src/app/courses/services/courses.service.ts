f4761243378ca64932a49bc1d0a839e3d5e362cc
João Pedro Moraes <joaopedromoraes@outlook.com>
2023-04-27 00:03:21 -0300

    feat: add update method

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { first, tap } from 'rxjs';
+import { first } from 'rxjs';
 
 import { Course } from '../model/course';
 
@@ -17,7 +17,20 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API).pipe(first());
   }
 
+  loadById(id: string) {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
   save(record: Partial<Course>) {
+    return record._id ? this.update(record) : this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
 }
